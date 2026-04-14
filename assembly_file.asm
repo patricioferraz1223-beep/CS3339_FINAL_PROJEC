3b551923b58906e9193b047bb810ae7c8c5075d9
Samuel Drell <iwn18@txstate.edu>
2026-04-14 10:54:48 -0500

    Adds example assembly file

---
+++ assembly_file.asm
@@ -0,0 +1,20 @@
+main:
+ADDI $t0 $zero 10
+ADDI $t1 $zero 3
+ADD  $t2 $t0 $t1
+SUB  $t3 $t0 $t1
+MUL  $t4 $t0 $t1
+AND  $t5 $t0 $t1
+OR   $t6 $t0 $t1
+SLL  $t7 $t1 2
+SRL  $s0 $t0 1
+SW   $t2 0($sp)
+LW   $s1 0($sp)
+BEQ  $s1 $t2 equal_label
+NOP
+J    end_label
+equal_label:
+ADDI $s2 $zero 1
+NOP
+end_label:
+NOP
