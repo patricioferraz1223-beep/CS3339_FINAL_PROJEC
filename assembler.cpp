2b30196d199a816865f53f3d03cfcce0bb313cd9
Samuel Drell <iwn18@txstate.edu>
2026-04-14 09:52:45 -0500

    Adds necessary instruction summary and code snippets

--- assembler.cpp
+++ assembler.cpp
@@ -1,5 +1,38 @@
 /*
 This file contains the assembler. Currently, I'm whiteboarding my design.
+
+
+
+
+
+// Create a text string, which is used to output the text file
+string instruction;
+
+// Read from the text file
+ifstream MyReadFile("filename.txt");
+
+// Use a while loop together with the getline() function to read the file line by line
+while (getline (MyReadFile, instruction)) {
+}
+
+// Close the file
+MyReadFile.close();
+
+
+Necessary instructions:
+    Opcode  Description
+    ADD     signed integer addition
+    ADDI    add immediate
+    SUB     signed integer subtraction
+    MUL     integer multiplication
+    AND     bitwise and operation
+    OR      bitwise or operation
+    SLL     shift left logical
+    SRL     shift right logical
+    LW      load word
+    SW      store word
+    BEQ     branch if equal to
+    J       jump
+    NOP     no op
+
 */
-
-
