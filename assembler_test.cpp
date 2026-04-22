d5462a6382542815a06eedc03e20b6e1877bf5e5
Samuel Drell <iwn18@txstate.edu>
2026-04-22 13:25:07 -0500

    Passes true to debug

--- assembler_test.cpp
+++ assembler_test.cpp
@@ -2,7 +2,7 @@
 #include "assembler.h"
 
 int main() {
-    assembler my_assembler;
+    assembler my_assembler(true);
     my_assembler.process_assembly_file("assembly_file.asm");
     return 0;
 }
