416b41e14f89ef51aacfc390d469dc1f615816ec
Samuel Drell <iwn18@txstate.edu>
2026-04-22 21:54:47 -0500

    Adds imem header

---
+++ imem.h
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <string>
+#include <stdexcept>
+
+/*/////////////////////////////////////////////////////////////////////////////////////////
+    Instruction Memory:
+    - INPUT: 32-bit instruction address (from PC)
+    - OUTPUT: 32-bit instruction
+//////////////////////////////////////////////////////////////////////////////////////////*/
+class InstructionMemory {
+    private:
+        std::vector<uint8_t> imem_data; // Vector to hold the instruction memory data
+
+        std::vector<uint8_t> load_binary(const std::string& filename);
+
+    public:
+        InstructionMemory();
+        InstructionMemory(const std::string& filename);
+
+        uint32_t read_address(uint32_t address);
+
+        void load_instructions(const std::string& filename);
+};
+
