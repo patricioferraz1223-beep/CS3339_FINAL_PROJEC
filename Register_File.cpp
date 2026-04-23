01c6f595c6a91f27361d1ebbbecc865d1dade76f
Samuel Drell <iwn18@txstate.edu>
2026-04-23 01:49:02 -0500

    Breaks REgFile into .h and .cpp

--- Register_File.cpp
+++ Register_File.cpp
@@ -8,38 +8,32 @@
     - OUTPUT: Read Data 1 (32 bits)
     - OUTPUT: Read Data 2 (32 bits)
 //////////////////////////////////////////////////////////////////////////////////////////*/
+#include "Register_File.h"
+#include <iomanip>
 
+RegisterFile::RegisterFile() {
+    for (int i = 0; i < 32; i++) registers[i] = 0;
+}
 
-class RegisterFile 
+void RegisterFile::read(uint8_t rs, uint8_t rt, uint32_t &data1, uint32_t &data2)
 {
-private:
-    uint32_t registers[32] = {0}; //Combining the number of registers and the value mapping variables
-    //Creates the 32 registers (all 32 bits wide intialized to 0)
+    data1 = registers[rs];
+    data2 = registers[rt];
+}
 
-public:
-    //Reading (on both register, which should happen twice per cycle)
-    void read(uint8_t rs, uint8_t rt, uint32_t &data1, uint32_t &data2)
+void RegisterFile::write(uint8_t rd, uint32_t data, bool regWrite) 
+{
+    if (regWrite && rd != 0)
     {
-        //Reading should happen combinationally, aka, instant without depending on a clock
-        data1 = registers[rs];
-        data2 = registers[rt];
+        registers[rd] = data;
     }
+}
 
-    //Writing (only one write per cycle, happens at the end of it, aka, sequentially)
-    void write(uint8_t rd, uint32_t data, bool regWrite) 
-    {
-        if (regWrite && rd != 0) //Checks for the Control Signal AND the destination register
-        {
-            registers[rd] = data; //Now it actually writes the data to a register
-        }
+void RegisterFile::print_registers() {
+    for (int i = 0; i < 32; i++) {
+        std::cout << "R" << std::setw(2) << i 
+                << ": 0x" << std::hex << std::setw(8) << std::setfill('0') 
+                << registers[i] << std::dec << std::setfill(' ')
+                << std::endl;
     }
-
-    void print_registers() {
-        for (int i = 0; i < 32; i++) {
-            std::cout << "R" << std::setw(2) << i 
-                    << ": 0x" << std::hex << std::setw(8) << std::setfill('0') 
-                    << registers[i] << std::dec << std::setfill(' ')
-                    << std::endl;
-        }
-    }
-};
+}
