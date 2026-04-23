1a40bcfa94357b3e00bbf81bedd47af698031a52
Samuel Drell <iwn18@txstate.edu>
2026-04-23 10:39:53 -0500

    Adds additional Assembly files

---
+++ ADDI_Only.asm
@@ -0,0 +1,20 @@
+main:
+ADDI $t0 $zero 10
+ADDI $t1 $zero 3
+ADDI $t2 $zero 13
+ADDI $t3 $zero 7
+ADDI $t4 $zero 30
+ADDI $t5 $zero 2
+ADDI $t6 $zero 4
+ADDI $t7 $zero 1
+ADDI $s0 $zero 20
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
+NOP
---
+++ NOP.asm
@@ -0,0 +1,22 @@
+ADDI $t0 $zero 10
+NOP
+NOP
+ADDI $t1 $zero 3
+NOP
+NOP
+ADD  $t2 $t0 $t1
+NOP
+NOP
+SUB  $t3 $t0 $t1
+NOP
+NOP
+AND  $t5 $t0 $t1
+NOP
+NOP
+OR   $t6 $t0 $t1
+NOP
+NOP
+SW   $t2 0($sp)
+NOP
+NOP
+LW   $s1 0($sp)
