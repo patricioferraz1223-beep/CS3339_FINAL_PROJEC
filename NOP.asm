032ae02f489794b3d5acf6a7aeac6ed934374f66
Samuel Drell <iwn18@txstate.edu>
2026-04-23 11:09:29 -0500

    Adds bubbled assembly file for hazard avoidance

--- NOP.asm
+++ NOP.asm
@@ -1,22 +1,29 @@
 ADDI $t0 $zero 10
 NOP
 NOP
+NOP
 ADDI $t1 $zero 3
+NOP
 NOP
 NOP
 ADD  $t2 $t0 $t1
 NOP
 NOP
+NOP
 SUB  $t3 $t0 $t1
+NOP
 NOP
 NOP
 AND  $t5 $t0 $t1
 NOP
 NOP
+NOP
 OR   $t6 $t0 $t1
 NOP
 NOP
+NOP
 SW   $t2 0($sp)
+NOP
 NOP
 NOP
 LW   $s1 0($sp)
